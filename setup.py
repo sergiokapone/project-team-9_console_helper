67693aac214dd49c583886eb8d142f747a6a7e9e
Sergiy <sergit.ponomarenko@gmail.com>
2023-04-29 08:46:56 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ setup(
     description="Command line helper",
     author="Team 9",
     license="MIT",
-    url='https://github.com/sergiokapone/project-team-9_console_helper/tree/dev/console_helper',
+    url="https://github.com/sergiokapone/project-team-9_console_helper/tree/dev/console_helper",
     include_package_data=True,
     packages=find_namespace_packages(),
     entry_points={"console_scripts": ["assistant = console_helper.CLI:main"]},
+    install_requires=["prettytable", "Pygments"],
 )
