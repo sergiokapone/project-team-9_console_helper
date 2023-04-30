536763d96338f870a234419d00bfe39b4143806a
Bohdan Datsko <bohdandatsko5@gmail.com>
2023-04-30 21:50:50 +0300

    search by id

--- console_helper/notebook.py
+++ console_helper/notebook.py
@@ -1,62 +1,90 @@
-import re
-import pathlib
-from collections import UserDict
-import pickle
-import csv
-from datetime import datetime, timedelta
+from collections import UserList
+from datetime import datetime
 
-class Note(UserDict):
-    def __init__(self):
-        super().__init__()
-        self.counter = 0
 
-    def set_counter(self):
-        max_id = 0
-        for tag, notes in self.data.items():
-            for note in notes:
-                if note["id"] > max_id:
-                    max_id = note["id"]
-        self.counter = max_id
+class Notebook(UserList):
+    def add_record(self, record):
+        self.data.append(record)
 
-    def add(self, tag, note_text):
-        max_id = max(
-            (note.get("id", 0) for notes in self.data.values() for note in notes),
-            default=-1,
-        )
+    def delete_record_by_id(self, record_id):
+        for order_number, value in enumerate(self.data):
+            if order_number == record_id:
+                self.data.pop(record_id)
+    """Здесь нужно добавить вывод сообщения с информацией об удаленной записи через return 'запись №record_id была удалена' или 'Запись не найдена'"""
 
-        note_id = max_id + 1
-        note = {"id": note_id, "text": note_text, "created": datetime.now()}
-        self.data.setdefault(tag, []).append(note)
-        self.counter = note_id
+    def find_record_by_tag(self, tag):
+        result = []  # list of notes with the requested tag
+        for record in self.data:
+            for tag_ in record.tags:
+                if tag == tag_.value:
+                    result.append(str(record))
 
-    def remove_note(self, note_id):
-        for tag, notes in self.data.items():
-            for i, note in enumerate(notes):
-                if note["id"] == int(note_id):
-                    del self.data[tag][i]
-                    break
-        for tag, notes in self.data.items():
-            for i, note in enumerate(notes):
-                if note["id"] > int(note_id):
-                    self.data[tag][i]["id"] -= 1
+        return result if result else f"Tag '{tag}' not found"
 
-    def display(self, tag=None):
-        return self
+    def find_record_by_text(self, text_part):
+        result = []
+        if text_part:
+            for record in self.data:
+                if text_part in record.note.value:
+                    result.append(str(record))
+            return result if result else f"The text '{text_part}' not found in notes"
+        return "You entered an empty string! Please Try again."
 
-    def find_notes(self, search_term):
-        result = {}
-        for tag, notes in self.data.items():
-            matching_notes = [
-                note
-                for note in notes
-                if search_term.lower() in str(note["text"]).lower()
-            ]
-            if matching_notes:
-                result[tag] = matching_notes
+    def find_record_by_date(self, date):
+        result = []
+        for record in self.data:
+            if date == record.date:  # date in format 30 April 2023
+                result.append(str(record))
+
+        return result if result else "No notes on this day"
+
+    def find_record_by_id(self, id_record):
+        result = []
+        id_found = False
+        for id, record in enumerate(self.data):
+            if id == id_record:
+                result.append(str(record))
+                id_found = True
+        if not id_found:
+            return 'Id not found'
         return result
+        
 
-    def sort_notes_by_tag(self):
-        sorted_notes = {}
-        for tag in sorted(self.data.keys()):
-            sorted_notes[tag] = self.data[tag]
-        return sorted_notes
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+
+class Tags(Field):
+    def __str__(self):
+        return self.value
+
+
+class Note(Field):
+    def __str__(self):
+        return self.value
+
+
+class Record:
+    def __init__(self, note):
+        self.note = Note(value=note)
+        self.tags = []
+        self.date = datetime.today().strftime("%d %B %Y")  # date in format 30 April 2023
+
+
+    def add_tag(self, tag):
+        self.tags.append(Tags(tag))
+
+    def change_note(self, new_note):
+        self.note = Note(value=new_note)
+
+    def __str__(self):
+        """Добавить окрашивание тегов в какой-нибуть цвет, чтобы визуально отделить их от основного текста"""
+        result = [self.note.value]
+        for tag in self.tags:
+            result.append(str(tag))
+        result.append(self.date)
+
+        return ", ".join(result)
+    
