23eacebd6f68b61da95611da154a871767dfae3d
halan10 <64984593+halan10@users.noreply.github.com>
2023-04-30 09:43:13 -0700

    add sorting functions

--- console_helper/notebook.py
+++ console_helper/notebook.py
@@ -3,8 +3,10 @@ from datetime import datetime
 
 
 class Notebook(UserDict):
-    id_count = 1
-
+    id_count = 1        
+    def __str__ (self)->str:
+        return self.value
+    
     def add_record(self, record):
         self.data[str(Notebook.id_count)] = record
         Notebook.id_count += 1
@@ -40,7 +42,19 @@ class Notebook(UserDict):
 
     def find_record_by_id(self, id_record):
         return self.data[str(id_record)] if str(id_record) in self.data else "Not found"
-
+        
+         #   вивід всіх тегів у нотатках
+    def show_all_tags(self):
+        result = []
+        for record in self.data.values():
+            for tag_ in record.tags:
+                result.append("#" + tag_ + " ")                            
+        for tag_ in result:          
+            if result.count(tag_) > 1:
+                result.remove(tag_)
+                return result
+        return f"Tags  not found"
+    
 
 class Field:
     def __init__(self, value):
@@ -55,33 +69,49 @@ class Tags(Field):
 class Record:
     def __init__(self, text):
         self.text = text
-        self.tags = set()
+        self.tags = []
         self.date = datetime.today().strftime("%d %B %Y")
 
     def add_tag(self, tag):
-        self.tags.add(Tags(tag))
+         self.tags.append(tag)
+        
 
     def change_text(self, new_text):
         self.text = new_text
-
+        
+    #   сортування тегів по алфавіту у нотатці    
+    def sort_tag(self,tag):
+          self.tags.sort()
+          
+    #   видалееня тегу з нотатки    
+    def remove_tag(self,tag):
+        self.tags.remove(tag)
+    
     def __str__(self):
         result = self.text + " ("
         for tag in self.tags:
             result += str(tag) + ", "
         return result + ") " + self.date
-
+    
+    def __repr__(self) -> str:
+        return self.text + " [" + ', '.join([p for p in self.tags]) +"] "+ self.date
 
 notebook = Notebook()
 
 record1 = Record("Some text")
-record2 = Record("Another some text")
-
+record2 = Record("Another some text ")
+record3 = Record("text ")
 record1.add_tag("work")
 record1.add_tag("job")
 record2.add_tag("work")
-
+record2.add_tag("eat")
+record2.add_tag("sport")
+record2.add_tag("our journey")
+record3.add_tag("journey")
 notebook.add_record(record1)
 notebook.add_record(record2)
-
+notebook.add_record(record3)
+# record2.sort_tag(record2.tags)
 record1.change_text("What doesn't kill you makes you stronger")
-
+# record2.remove_tag("sport")
+# print(notebook.show_all_tags())
