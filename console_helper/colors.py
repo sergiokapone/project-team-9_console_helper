632f32a40cbcd32f1b784a80e4f51597cd2ac159
Sergiy <sergit.ponomarenko@gmail.com>
2023-05-02 20:10:53 +0300

    fix colors

--- console_helper/colors.py
+++ console_helper/colors.py
@@ -1,6 +1,7 @@
-G = "\033[0;32;40m"  # GREEN
-B = "\033[96m"  # Blue
-P = "\033[95m"  # Pink
-R = "\033[1;31m"  # Red
+G = "\033[1;92m"  # GREEN
+B = "\033[1;96m"  # Blue
+P = "\033[4;95m"  # Pink
+R = "\033[1;91m"  # Red
 N = "\033[0m"  # Reset
-Y = "\033[0;33;40m"  # Yellow
+Y = "\033[0;93m"  # Yellow
+W = "\033[97m"  # White
