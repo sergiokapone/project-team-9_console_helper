c84604ddeb7ff30f6b88519f9165b75b07522907
Sergiy <sergit.ponomarenko@gmail.com>
2023-05-02 20:46:52 +0300

    change good_bye

--- console_helper/CLI.py
+++ console_helper/CLI.py
@@ -59,7 +59,7 @@ def good_bye(*args):
     os.system("cls" if os.name == "nt" else "clear")
     save(CONTACT_FILE)
     save_notes(NOTES_FILE)
-    return "Good bye!"
+    return "Good bye! See you later.\nDotn't warry, your data was saved."
 
 
 @input_error
